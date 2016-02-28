b7e4356025ef7fbe9b7e7f35292152049900d447
vyiii <ivylinlaw@10.123.103.166>
2016-02-27 17:29:17 -0800

    Changes from Ivy

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 	public static void main(String [] args) {
 		System.out.println("Hello World");
 		System.out.println("CS201");//test
+		System.out.print("Hello from the outside");
 	}
 }
